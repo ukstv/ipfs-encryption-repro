74a7b66f790b5c560afb616b7cc81e8e49d7d82c
ukstv <sergey@ukstv.me>
2021-05-27 15:18:59 +0300

    1-bis

--- run-b.js
+++ run-b.js
@@ -1,7 +1,8 @@
 const ipfsHttpClient = require('ipfs-http-client')
 
 async function main() {
-    const client = await ipfsHttpClient.create({url: 'http://localhost:5012'})
+    // const client = await ipfsHttpClient.create({url: 'http://localhost:5012'})
+    const client = ipfsHttpClient({url: 'http://localhost:5012'})
     console.log('id', (await client.id()).id)
     setInterval(async () => {
         console.log('peers', await client.swarm.peers())
